4084c213f637f8a3ccf9321a0a54bfcf93b08268
Victor Levesque <vlevesque@intershop.com.au>
2025-01-28 15:49:05 +1100

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,28 +13,11 @@ provider "digitalocean" {
   token = var.digitalocean_token
 }
 
-resource "digitalocean_ssh_key" "default" {
-  name       = "my-key"
-  public_key = var.ssh_public_key
-}
-
 resource "digitalocean_droplet" "nginx_droplet" {
   name   = "nginx-server"
   region = "nyc3" # Closest to Montreal
   size   = "s-1vcpu-1gb" # Smallest droplet
   image  = "ubuntu-22-04-x64"
-
-  ssh_keys = [digitalocean_ssh_key.default.id]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    apt-get update && apt-get install -y apt-transport-https curl docker.io
-    curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-    chmod +x kubectl && mv kubectl /usr/local/bin/
-    curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-    chmod +x minikube && mv minikube /usr/local/bin/
-    usermod -aG docker root
-  EOF
 
   tags = ["nginx", "minikube"]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,1 @@
 variable "digitalocean_token" {}
-
-variable "ssh_private_key" {
-  description = "The private SSH key"
-  type        = string
-}
-
-variable "ssh_public_key" {
-  description = "The public SSH key"
-  type        = string
-}
-
